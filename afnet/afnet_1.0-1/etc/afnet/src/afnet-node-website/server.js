c36c1a48f03193e79a4113156c0b9677849fdf71
Gavin Rogers <mgavinr@gmail.com>
2021-05-23 15:00:13 +0100

    PD-None: nothing really.............................

--- afnet/afnet_1.0-1/etc/afnet/src/afnet-node-website/server.js
+++ afnet/afnet_1.0-1/etc/afnet/src/afnet-node-website/server.js
@@ -23,51 +23,81 @@ var afnet = {
   stoppedMessage : "",
   runningfile : process.env.AFHOME + "/" + process.env.AF_TEXT_RUNNING,
   startfile : process.env.AFHOME + "/" + process.env.AF_TEXT_START,
-  stopfile : process.env.AFHOME + "/" + process.env.AF_TEXT_STOP,
+  stoppedfile : process.env.AFHOME + "/" + process.env.AF_TEXT_STOP,
+  servers: [],
   runningServers: [],
   startServers: [],
   stoppedServers: [],
-  runningServersHeader: ["Hostname", "Login", "Remote Directory", "Local Directory"],
-  startServersHeader:   ["Hostname", "Login", "Remote Directory", "Local Directory"],
-  stoppedServersHeader: ["Hostname", "Login", "Remote Directory", "Local Directory"],
+  runningServersHeader: ["Hostname", "Login", "Remote Directory", "Local Directory", "Status"],
+  startServersHeader:   ["Hostname", "Login", "Remote Directory", "Local Directory", "Status"],
+  stoppedServersHeader: ["Hostname", "Login", "Remote Directory", "Local Directory", "Status"],
+  index: {"hostname":0, "login": 1, "remotedir": 2, "localdir": 3, "status":4},
   getLocalDirectory : function(file_entry_list) {
-    var dir = process.env.AFNETWORK+"/"+file_entry_list[0]+"/"+file_entry_list[2];
+    var dir = process.env.AFNETWORK+"/"+file_entry_list[this.index["hostname"]]+"/"+file_entry_list[this.index["remotedir"]];
     dir = dir.replace(/:/g, "")
     return dir;
   },
-  getServers : function(filename) {
+  readServerFile : function(filename) {
     var file_contents = read.sync(filename, 'utf8');
-    var servers = []
+    var server_list = []
+    log.debug("Reading file " + filename);
     for(file_entry of file_contents.split("\n")) {
       file_entry = file_entry.trim();
       if(file_entry.length == 0) continue;
       var file_entry_list = file_entry.split(" ");
-      var new_file_entry_list = file_entry_list.concat([this.getLocalDirectory(file_entry_list)]);
-      servers.push(new_file_entry_list);
+      var expected_length = Object.keys(this.index).length;
+      if (file_entry_list[this.index["localdir"]] == undefined) {
+        file_entry_list = file_entry_list.concat([this.getLocalDirectory(file_entry_list)]);
+        log.debug("Adding column for localdir: "+ file_entry_list[this.index["localdir"]]);
+      }
+      if (file_entry_list[this.index["status"]] == undefined) {
+        file_entry_list = file_entry_list.concat(["unknown"]);
+        log.debug("Adding column for status: "+ file_entry_list[this.index["status"]]);
+        //log.debug(file_entry_list);
+      }
+      if (file_entry_list.length == expected_length) {
+        server_list.push(file_entry_list);
+      } else {
+        log.error("Entry in " + filename + " invalid ignoring: " + file_entry_list);
+        log.error(file_entry_list.length);
+        log.error(expected_length);
+      }
     }
-    return servers;
+    return server_list;
   },
-  updateServersAsync : function(filename, value) {
+  writeServerFile : function(filename, contents) {
+    var file_contents = ""
+    for(var i=0; i < contents.length; i++) {
+      file_contents += contents[i].join(' ')+'\n';
+    }
+    try {
+      const data = fs.writeFileSync(filename, file_contents)
+      log.debug("writeServerFile("+filename+") ok");
+    } catch (err) {
+      log.error(err)
+    }
+  },
+  addServerEntryAsync : function(filename, value) {
     fs.appendFile(filename, value, (err) => {
-      if (err) console.log(err);
-      console.log("Added=" + value + " to filename=" + filename);
+      if (err) log.error(err);
+      log.debug("Added=" + value + " to filename=" + filename);
     });
   },
-  updateServers : function(filename, value) {
+  addServerEntry : function(filename, value) {
     try {
       fs.appendFileSync(filename, value);
-      console.log("Added=" + value + " to filename=" + filename);
+      log.debug("Added=" + value + " to filename=" + filename);
     } catch(err) {
-      console.log(err);
+      log.error(err);
     }
   },
   getConfiguredServers : function() {
     runningfile = process.env.AFHOME + "/" + process.env.AF_TEXT_RUNNING,
     startfile = process.env.AFHOME + "/" + process.env.AF_TEXT_START,
-    stopfile = process.env.AFHOME + "/" + process.env.AF_TEXT_STOP,
-    this.runningServers = this.getServers(this.runningfile);
-    this.startServers = this.getServers(this.startfile);
-    this.stoppedServers = this.getServers(this.stopfile);
+    stoppedfile = process.env.AFHOME + "/" + process.env.AF_TEXT_STOP,
+    this.runningServers = this.readServerFile(this.runningfile);
+    this.startServers = this.readServerFile(this.startfile);
+    this.stoppedServers = this.readServerFile(this.stoppedfile);
   }
 };
 
@@ -83,7 +113,6 @@ app.get('/', (req, res) => {
     afnet: afnet,
   });
 });
-
 
 app.get('/home', (req, res) => {
   afnet.getConfiguredServers();
@@ -101,16 +130,10 @@ app.get('/start', (req, res) => {
 
   if (req.query.indexname === "stopped") {
     afnet.stoppedMessage = message;
-    var data = afnet.stoppedServers[req.query.index].slice(0,3).join(' ')+'\n';
-    afnet.updateServers(afnet.startfile, data);
-    // TODO remove from the stop file!
-    console.log(message);
-  } else {
-    afnet.configuredMessage = message;
-    var data = afnet.startServers[req.query.index].slice(0,3).join(' ')+'\n';
-    afnet.updateServers(afnet.startfile, data);
-    // TODO remove from the stop file!
-    console.log(message);
+    var entry = afnet.stoppedServers[req.query.index].join(' ')+'\n';
+    afnet.stoppedServers.splice(req.query.index, 1);
+    afnet.writeServerFile(afnet.stoppedfile, afnet.stoppedServers);
+    log.info("Removing an entry from the stop file: " + entry);
   }
 
   res.render('run.ejs', {
@@ -125,16 +148,12 @@ app.get('/stop', (req, res) => {
   afnet.runningMessage = "";
   afnet.stoppedMessage = "";
 
-  if (req.query.indexname === "running") {
-    afnet.runningMessage = message;
-    var data = afnet.stoppedServers[req.query.index].slice(0,3).join(' ')+'\n';
-    afnet.updateServers(afnet.stopfile, data);
-    console.log(message);
-  } else {
-    afnet.configuredMessage = message;
-    var data = afnet.startServers[req.query.index].slice(0,3).join(' ')+'\n';
-    afnet.updateServers(afnet.stopfile, data);
-    console.log(message);
+  if (req.query.indexname === "start") {
+    afnet.startMessage = message;
+    var entry = afnet.startServers[req.query.index].slice(0,3).join(' ')+'\n';
+    afnet.addServerEntry(afnet.stoppedfile, entry);
+    log.debug(message);
+    log.info("Adding an entry to the stop file: " + entry);
   }
 
   res.render('run.ejs', {
@@ -147,4 +166,5 @@ app.get('/stop', (req, res) => {
 
 const server = app.listen(7000, () => {
   console.log(`Express running → PORT ${server.address().port}`);
+  log.info(`Express running → PORT ${server.address().port}`);
 });
