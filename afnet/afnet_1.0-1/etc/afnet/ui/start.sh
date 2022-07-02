762db972b42db043bc45bc7129f014fc43e11573
Gavin Rogers <mgavinr@gmail.com>
2022-07-03 00:30:00 +0100

    PD-None: add apt and yum...............

--- afnet/afnet_1.0-1/etc/afnet/ui/start.sh
+++ afnet/afnet_1.0-1/etc/afnet/ui/start.sh
@@ -1,3 +1,8 @@
 #export DEBUG="afnet-server.js:(debug|info)"
+which node || (which yum && sudo yum install node)
+which node || (which apt && sudo apt install nodejs)
+which npm || (which yum && sudo yum install npm)
+npm install .
 export DEBUG="afnet-server.js:*"
-node server.js
+which node && node server.js
+which node || nodejs server.js
